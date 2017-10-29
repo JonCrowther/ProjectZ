501e8d5a5d1adc04be690686781954650cdf21fb
Jonathan Crowther <jkecrowther@gmail.com>
2017-10-29 19:07:32 -0400

    Improved movement highlighter
    Made the movement highlighter more efficient by making the creation happen on movement, instead of on mouse over

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,30 +8,42 @@ public class Player : MonoBehaviour {
 	public GameObject moveHighlighter;
 	public int speed = 2;
 
+	private List<GameObject> movableTiles = new List<GameObject>();
+
 	private void Update() {
 		if (Input.GetButtonUp ("Fire1")) {
 			transform.position = selector.transform.position;
-		}
-	}
 
-	void OnMouseEnter()
-	{
-		for (int x = -speed; x <= speed; x++) {
-			for (int y = -speed; y <= speed; y++) {
-				if ((Mathf.Abs (x) + Mathf.Abs (y)) <= speed) {
-					if((transform.position.x + x < BoardManager.rows && transform.position.x + x >= 0) 
-						&& (transform.position.y + y < BoardManager.columns && transform.position.y + y >= 0))
-					Instantiate (moveHighlighter, new Vector3 (transform.position.x + x, transform.position.y + y, 0f), Quaternion.identity);
+			for (var i = 0; i < movableTiles.Count; i++) {
+				Destroy (movableTiles [i]);
+			}
+
+			movableTiles.Clear ();
+
+			for (int x = -speed; x <= speed; x++) {
+				for (int y = -speed; y <= speed; y++) {
+					if ((Mathf.Abs (x) + Mathf.Abs (y)) <= speed) {
+						if ((transform.position.x + x < BoardManager.rows && transform.position.x + x >= 0)
+							&& (transform.position.y + y < BoardManager.columns && transform.position.y + y >= 0)) {
+							movableTiles.Add(Instantiate (moveHighlighter, new Vector3 (transform.position.x + x, transform.position.y + y, 0f), Quaternion.identity));
+						}
+					}
 				}
 			}
 		}
 	}
 
+	void OnMouseEnter()
+	{
+		for (var i = 0; i < movableTiles.Count; i++) {
+			movableTiles [i].SetActive (true);
+		}
+	}
+
 	void OnMouseExit()
 	{
-		GameObject[] oldHighlights = GameObject.FindGameObjectsWithTag ("Highlight");
-		foreach (GameObject highlight in oldHighlights) {
-			Destroy (highlight);
+		for (var i = 0; i < movableTiles.Count; i++) {
+			movableTiles [i].SetActive (false);
 		}
 	}
 
